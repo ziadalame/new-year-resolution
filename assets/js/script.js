2a8549f9ddf333cf048c308a65ad728a4c8fa6dd
RamiAwar <rami.awar.ra@gmail.com>
2014-12-28 02:38:29 +0200

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,63 @@
 
-jQuery(document).ready(function($) {
-	headerSize();
-	fontSize();
-	paddingTop();
-	modalSize();
-	$(window).resize(function(event) {
-		headerSize();
-		fontSize();
-		paddingTop();
-		modalSize();
+jQuery(document).ready(function($) {		//ONDOCUMENT READY
+
+
+				headerSize();		//RESIZING HEADER ACCORDING TO SCREEN WIDTH
+				fontSize();			//RESIZING FONT SIZE ACCORDING TO SCREEN WIDTH 
+				paddingTop();		
+				modalSize();
+				$("#signup_alert").hide(1);
+				$("#login_alert").hide(1);
+				$("#email_display").hide(1);
+
+
+
+	$(window).resize(function(event) {			//DOING THE SAME RESIZES ON WINDOW RESIZING
+
+				headerSize();
+				fontSize();
+				paddingTop();
+				modalSize();
+
 	});
 	
 
-	$("#login-form").submit(function(event){
+	$("#login-form").submit(function(event){		//ONSUBMIT LOGIN
 
 		event.preventDefault();
-		console.log('form submitted');
+
+
 	});
 
 
 	$('#login-form').parsley({
+
 		trigger:      'change',
 		successClass: "has-success",
 		errorClass: "has-error",
 		classHandler: function (el) {
             return el.$element.closest('.form-group'); //working
         }
+
+
     });
 
 	$('#signup-form').parsley({
+
 		trigger:      'change',
 		successClass: "has-success",
 		errorClass: "has-error",
 		classHandler: function (el) {
             return el.$element.closest('.form-group'); //working
         }
+
     });
 
-		$("#submit_signup").click(function(){
-			var email = $("#signup_email").val();
-			var password = $("#signup_password").val();
-// Returns successful data submission message when the entered information is stored in database.
-			 dataString = {'email_signup':email, 'password_signup':password,'submit_signup':true};
+	$("#submit_signup").click(function(){
+
+		var email = $("#signup_email").val();
+		var password = $("#signup_password").val();
+		dataString = {'email_signup':email, 'password_signup':password,'submit_signup':true};
 
 
 		if(email==''||password=='')
@@ -51,70 +67,98 @@ jQuery(document).ready(function($) {
 		else
 		{
 			console.log(dataString);
-		// AJAX Code To Submit Form.
-		$.ajax({
-			type: "POST",
-			url: "process.php",
-			data: dataString,
-			success: function(result){
-				console.log(result);
-			}
-		});
-		}
-		return false;
-		});
-
-
-		$("#submit_login").click(function(){
-			var email = $("#email_login").val();
-			var password = $("#password_login").val();
-
-			dataString = {'email_login':email, 'password_login':password,'submit_login':true};
-
-			if (email=='' || password=='')
-			{
-				alert("Please fill in all the fields");
-			}
-			else{
-				console.log("Input correct so far.");
-				$.ajax({
-					type:"POST",
-					url:"process.php",
-					data: dataString,
-					success: function(result){
+			// AJAX Code To Submit Form.
+			$.ajax({
+				type: "POST",
+				url: "process.php",
+				data: dataString,
+				success: function(result){
+						// JSON.parse(result);
 						console.log(result);
 					}
 				});
-			}
-			return false;
-
-		});
-
+		}
+		return false;
+	});
 
 
+	$("#submit_login").click(function(){
+
+		var email = $("#email_login").val();
+		var password = $("#password_login").val();
+
+		dataString = {'email_login':email, 'password_login':password,'submit_login':true};
+
+		if (email=='' || password=='')
+		{
+			alert("Please fill in all the fields");
+		}
+		else{
+
+			console.log("Input correct so far.");
+			$.ajax({
+				type:"POST",
+				url:"process.php",
+				data: dataString,
+				success: function(result){
+
+					var result_object = $.parseJSON(result);
+
+					if (result_object.login_success){
+
+						$("#login").modal('hide');
+						$("#login_alert").hide(1000);
+						$(".username_button").hide(1000);
+						$("#email_display").html(email);
+						$("#email_display").show(1000);
+						
 
 
+					}else{
+
+						$("#login_alert").show(1000);
+					}
+
+				}
+			});
+		}
+		return false;
+
+	});
 
 
 
 });
+
+
+
 var screen_height = $(window).height();
 var screen_width = $(window).width();
 
+
 function headerSize(){
+
 	$("#header").css('min-width', screen_width);
 	$("#header").css('min-height', screen_height);
+
 }
 
+
+
+
 function modalSize(){
+
 	if(screen_width < 358){
+
 		$('.modal-body').css('width', screen_width-15);
 		$('.modal-dialog').css('width', screen_width-20);
 		$('.modal-body').css('padding-left', 15);
 
 
 	}
+
 	if(screen_width > 358){
+
 		$('.modal-body').css('width', 358);
 		$('.modal-dialog').css('width', 358);
 		$('.modal-body').css('padding', 15)
@@ -124,48 +168,39 @@ function modalSize(){
 
 
 function fontSize(){
+
 	if( screen_width < 602 ){
+
 		$(".question").css('text-align', "center");
-		$(".question").css('font-size', 31)
+		$(".question").css('font-size', 31);
+
 	}
+
 	if( screen_width > 602){
+
 		$(".question").css('text-align', "left");
+
 	}
 
 }
 function paddingTop(){
+
 	if(screen_width < 359){
+
 		$(".reminder").css('padding-top',0);
 		$(".reminder").css('text-align',"center");
+	
 	}
+
 	if(screen_width >= 359){
+
 		$(".reminder").css('padding-top', 15)
 		$(".reminder").css('text-align', "right");
+	
 	}
 	
 }
 
 
-$( "#login-form" ).submit(function( event ) {
-});
 
 
-function validateEmail(email) { 
-	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	return re.test(email);
-} 
-
-function checkPwd(str) {
-	if (str.length < 6) {
-		return("too_short");
-	} else if (str.length > 20) {
-		return("too_long");
-	} else if (str.search(/\d/) == -1) {
-		return("no_num");
-	} else if (str.search(/[a-zA-Z]/) == -1) {
-		return("no_letter");
-	} else if (str.search(/[^a-zA-Z0-9\!\@\#\$\%\^\&\*\(\)\_\+]/) != -1) {
-		return("bad_char");
-	}
-	return "ok";
-}
--- process.php
+++ process.php
@@ -1,88 +1,162 @@
-<html>
-<head>
-	<title></title>
-</head>
-<body>
+<?php
 
-	<?php
-
-		$username = "root";
-		$password = "";
-		$hostname = "localhost";
-		$database = 'nyr';
+$username = "root";
+$password = "root";
+$hostname = "localhost";
+$database = 'nyr';
 
 
-		$connection =mysqli_connect($hostname, $username, $password, $database); // Establishing Connection with Server..
-		$db = mysqli_select_db($connection, "nyr"); // Selecting Database
-		//Fetching Values from URL
+$general_return = [
+	"db_connect_success" => false,
+	"login_success" => false,
+	"new_account_creation" => false
+];
 
 
-			if(mysqli_connect_errno()) {
-			echo "Error: Could not connect to database.";
-			exit;
+$connection =mysqli_connect($hostname, $username, $password, $database); 		// Establishing Connection with Server..
+$db = mysqli_select_db($connection, "nyr"); 		// Selecting Database
+
+
+	//Fetching Values from URL
+
+
+if(mysqli_connect_errno()) {
+
+	echo "Error: Could not connect to database.";
+	exit;
+
+}else{
+	$general_return["db_connect_success"] = true;
+}
+
+
+
+	//SIGNUP FORM //SIGNUP * SIGNUP * SIGNUP * SIGNUP * SIGNUP * SIGNUP * SIGNUP * SIGNUP SIGNUP 
+
+
+
+
+
+
+
+if(isset($_POST['submit_signup'])){		//CHECK IF FORM IS SUBMITTED
+
+
+	if(isset($_POST['email_signup']) && isset($_POST['password_signup'])){		//CHECK IF EMAIL OR PASSWORD IS EMPTY
+
+
+		$email = mysqli_real_escape_string($connection, $_POST['email_signup']);		//CREATE EMAIL VAR
+		$password = md5(mysqli_real_escape_string($connection, $_POST['password_signup']));		//CREATE PASS VAR
+
+	}
+
+	//FORM SUBMITTED
+
+	//CHECKING FOR DUPLICATES
+
+
+	$query = "SELECT * FROM information WHERE (email = '$email')";
+	$check = mysqli_query($connection, $query);
+	$checkrows = mysqli_num_rows($check);
+
+	
+
+
+
+	if ($checkrows == 1){
+		
+		echo "This username is already registered";
+	
+	}else{
+
+		$query = "INSERT INTO information (email, password) VALUES ('$email','$password')";
+		$result = mysqli_query($connection, $query);
+		$general_return["new_account_creation"] = true;
+		
+
+
+		if (mysqli_query($connection, $result)) {
+
+				echo "SUCCESSFULLY INSERTED INTO DATABASE"; 	 //RESULT INSERTION SUCCESS MESSAGE
+
 			}else{
-				echo "Connected to database - ";
+				
+				echo "COULDNT INSERT INTO DATABASE"  ;  	// SHOW ERROR IF RESULT INSERTION DID NOT WORK
+			
+
 			}
+		
+
+		echo "FORM SUCCESSFULLY SUBMITTED"; 
+	
+	}
+
+}
+
+
+		//LOGIN * LOGIN * LOGIN * LOGIN * LOGIN
 
 
 
-			//SIGNUP FORM
+if(isset($_POST['submit_login'])){		//CHECK IF SUBMIT CONTAINS SOMETHING
+	
 
-			if(isset($_POST['submit_signup'])){//CHECK IF FORM IS SUBMITTED
-				if(isset($_POST['email_signup']) && isset($_POST['password_signup'])){//CHECK IF EMAIL OR PASSWORD IS EMPTY
-					$email = mysqli_real_escape_string($_POST['email_signup']);//CREATE EMAIL VAR
-					$password = md5(mysqli_real_escape_string($connection, $_POST['password_signup']));//CREATE PASS VAR
-				}
+	if(isset($_POST['email_login']) && isset($_POST['password_login'])){		//CHECK IF USERNAME AND PASSWORD CONTAIN SOMETHING
+		
 
-				//FORM SUBMITTED
 
-					$query = "INSERT INTO information (email, pass) VALUES ('$email','$password')";
-					$result = mysqli_query($connection, $query);
-					if (mysqli_query($connection, $result)) {
-							echo "SUCCESSFULLY INSERTED INTO DATABASE";    //RESULT INSERTION SUCCESS MESSAGE
-						} else {
-							echo "COULDNT INSERT INTO DATABASE"  ;  // SHOW ERROR IF RESULT INSERTION DID NOT WORK
-						}
-				echo "FORM SUCCESSFULLY SUBMITTED";
+		$email = mysqli_real_escape_string($connection, $_POST['email_login']);					//DEFINE EMAIL VARIABLE
+		$password = md5(mysqli_real_escape_string($connection, $_POST['password_login']));		//DEFINE PASSWORD VARIABLE
+		$query = "SELECT * FROM information WHERE (email = '$email')";							//GET SHIT FROM TABLE WHERE THE EMAIL IS PRESENT
+		$result = mysqli_query($connection, $query);											//SAVE SHIT INTO QUERIED RESULT
+		$query = "SELECT * FROM information WHERE (email = '$email')";
+		$check = mysqli_query($connection, $query);
+		$checkrows = mysqli_num_rows($check);
+
+		if($checkrows==0){
+
+			echo json_encode($general_return);
+
+		}
+
+		while ($row = $result->fetch_assoc()) {		//CHECK PASSWORD ELIGIBILITY
+			
+
+			if($row["password"] == $password){
+
+				$general_return['login_success'] = true;
+
+
+				echo json_encode($general_return);
+
 			}else{
-				echo "FORM SUBMIT FAILED";
+
+				echo json_encode($general_return);
+
 			}
+		}
+		
 
 
-			//LOGIN SHIT
-			if(isset($_POST['submit_login'])){//CHECK IF SUBMIT CONTAINS SOMETHING
-				if(isset($_POST['email_login']) && isset($_POST['password_login'])){//CHECK IF USERNAME AND PASSWORD CONTAIN SOMETHING
-					$email = mysqli_real_escape_string($_POST['email_login']);//DEFINE EMAIL VARIABLE
-					$password = md5(mysqli_real_escape_string($connection, $_POST['password_login']));//DEFINE PASSWORD VARIABLE
-
-					$query = "SELECT * FROM information WHERE (email = '$email')";//GET SHIT FROM TABLE WHERE THE EMAIL IS PRESENT
-					var_dump($query);
-					$result = mysqli_query($connection, $query);//SAVE SHIT INTO QUERIED RESULT
-					var_dump($result);
-					while ($row = $result->fetch_assoc()) {//CHECK PASSWORD ELIGIBILITY
-				        if($row["pass"] == $password){
-				        	echo "You are now logged in";
-				        }else{
-				        	echo "Incorrect username or password.";
-				        }
-				    }
-					$rowcount=mysqli_num_rows($result);
-
-					if($rowcount == 1){
-						var_dump($result);
-					}else{
-						var_dump($rowcount);
-					}
-
-
-				}
-			}
+		$rowcount=mysqli_num_rows($result);
 
 		
-		mysqli_close($connection); // Connection Closed
-		?>
+		if($rowcount == 1){
+			
+			// var_dump($result);
+
+		}else{
+
+			// var_dump($rowcount);
+
+		}
 
 
+	}
+}
 
-</body>
-</html>
+
+mysqli_close($connection); // Connection Closed
+
+
+?>
